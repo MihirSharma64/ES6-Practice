bd93150e20db4084ea4f7c487aac9579207d4162
MihirSharma64 <mihirsharma64@gmail.com>
2021-06-20 14:46:09 +0530

    Added comments

--- Spread/Arrays.js
+++ Spread/Arrays.js
@@ -10,11 +10,11 @@ let arr2 = [6,7,8,9];
 
 // let narr = [...arr1,5,...arr2];
 // console.log(narr);
-// Agar traditional(push) tareeke se krte to mutatively hote(ussi arr mei change hota h), yeh immutable type tareeka hai..React mei mutable ops nhi krte.
+// Agar traditional(push) tareeke se krte to mutatively hote(ussi arr mei change hota h baar baar push krke), yeh immutable type tareeka hai..React mei mutable ops nhi krte.
 
 
-// Arrays heap pe banti hai
-// let copy = arr1;
+// Arrays heap pe banti hai 
+// let copy = arr1; copy and arr1 dono same address pe point krenge
 // copy[0] = 4;
 // console.log(copy);
 // console.log(arr1);
